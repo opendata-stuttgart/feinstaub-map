d565af85f3318d68be06e238f10c2cbd33488003
rashfael <rashfael@isobeef.org>
2016-04-23 16:55:26 +0200

    init with skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+export default {
+	template: require('./app.jade'),
+	components: {},
+	data() {
+		return {
+		}
+	},
+	methods: {
+
+	},
+	ready() {
+		
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	entry: [
+		'babel-polyfill',
+		'./src/style.styl',
+		'./src/main',
+		// 'webpack-dev-server/client?http://localhost:8080'
+	],
+	output: {
+			publicPath: '/',
+			filename: 'main.js'
+	},
+	debug: true,
+	devtool: 'source-map',
+	module: {
+		loaders: [
+			{ test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel-loader', query: { presets: ['es2015'] }},
+			{ test: /\.jade$/, loader: "html!jade-html" },
+			{ test: /\.styl$/, loader: 'style!css!stylus' }
+			// { test: /\.less$/, loader: "style!css!autoprefixer!less" },
+		]
+	},
+	devServer: {
+		contentBase: "./src"
+	},
+  plugins: [new HtmlWebpackPlugin({
+		template: './src/index.html'
+	})],
+	stylus: {
+		use: [require('nib')(),require('axis')(),require('rupture')(),require('autoprefixer-stylus')()]
+	}
+};
