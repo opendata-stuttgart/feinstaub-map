7c59cd5f84d1abef0b2f97a6693b3e2e253ed910
rashfael <rashfael@isobeef.org>
2016-12-13 18:50:06 +0100

    change default zoom lvl

--- src/map.vue
+++ src/map.vue
@@ -13,7 +13,7 @@ export default {
 		this.$nextTick(() => {
 			let map = leaflet.map(this.$el, {
 				center: config.center,
-				zoom: 13
+				zoom: 11
 			})
 			leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
