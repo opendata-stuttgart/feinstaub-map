bf49dbf172b3f669b8065a2b2e545bff6c8cf1d3
rashfael <rashfael@isobeef.org>
2017-01-31 17:28:42 +0100

    optionally parse center coords from url

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import querystring from 'querystring'
 import Main from './main.vue'
 import './style.styl'
+import config from 'config'
 
+// parse url parameters
+
+const query = querystring.parse(window.location.search.substring(1))
+
+console.log(query)
+
+if (query.center) {
+	const center = query.center.split(',').map((coord) => parseFloat(coord))
+	config.center = center
+}
 /* eslint-disable no-new */
 
 new Vue(Main).$mount('#v-app')
