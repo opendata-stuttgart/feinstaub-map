4f4c3da805e1789ea8555487af8a026fe3478c76
Rajko Zschiegner <ricki_z@gmx.de>
2017-02-07 23:28:29 +0100

    just another bunch of cities

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ if (query.center) {
 } else {
 	var towns = new Array();
 	var zooms = new Array();
+//	Germany
 	towns["stuttgart"] = [48.800000,9.200000];
 	towns["augsburg"] = [48.3585423,10.8943915];
 	towns["berlin"] = [52.516666666,13.383333333];
@@ -26,6 +27,7 @@ if (query.center) {
 	towns["greiz"] = [50.6535563,12.1884156];
 	towns["freiburg"] = [47.990000,7.820000];
 	towns["hamburg"] = [53.5464148,9.984099];
+	towns["hannover"] = [52.374444444444,9.7386111111111];
 	towns["jena"] = [50.9237905,11.5806506];
 	towns["konstanz"] = [47.660000,9.160000];
 	towns["muenchen"] = [48.140000,11.560000];
@@ -33,7 +35,18 @@ if (query.center) {
 	towns["oldenburg"] = [53.143888888,8.213888888];
 	towns["paderborn"] = [51.7231179,8.7548663];
 	towns["regensburg"] = [49.016666666,12.083333333];
+	towns["rostock"] = [54.083333333333,12.133333333333];
 	towns["ulm"] = [48.3843119,10.000000];
+	towns["wismar"] = [53.888213888889,11.462016666667];
+
+//	Schweiz
+	towns["basel"] = [47.55814,7.58769];
+	towns["zuerich"] = [47.37174,8.54226];
+
+//	Poland
+	towns["wroclaw"] = [51.11,17.032222222222];
+
+//  country level maps
 	towns["taiwan"] = [23.62985,121.05317]; zooms["taiwan"] = 9;
 
 	var hostname = location.hostname;
